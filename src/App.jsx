cd24dd7bfe85cdf8975e54c1e7d52992d0d7fc36
Zubair Sidhu <sidhuzubair@gmail.com>
2021-11-10 11:33:01 -0800

    Remove antdesign components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Button, DatePicker} from 'antd';
 import './App.css';
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          *AntDesign provides a set of high-quality React components out of the box!*
+          *AntDesign provides a set of high-quality React components out of the box!!*
           <br />
           <a href="https://ant.design/components/overview/" target="_blank" rel="noreferrer">
             Overview of available AntDesign components
@@ -16,10 +15,6 @@ function App() {
         </p>
         <br />
         <h1 style={{color: 'white'}}>Example Ant Design Components</h1>
-        <Button type="primary" onClick={() => alert('you clicked me!')}>
-          dummy antdesign button
-        </Button>
-        <DatePicker placeholder="select date" />
       </header>
     </div>
   );
