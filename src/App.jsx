cf8578ef35f5af15ff96045d2bb13ec5e4a29b23
Rashmi Sharma <rashmisharma489@gmail.com>
2021-11-10 10:07:43 -0800

    installed antdesign, add couple components to start page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          *AntDesign provides a set of high-quality React components out of the box!*
+          *AntDesign provides a set of high-quality React components out of the box*
           <br />
           <a href="https://ant.design/components/overview/" target="_blank" rel="noreferrer">
             Overview of available AntDesign components
