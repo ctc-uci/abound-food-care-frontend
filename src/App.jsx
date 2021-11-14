dd37938b3cde757318cc0ec2aca8ba5883e5b213
Angeline Pho <apho8884@uci.edu>
2021-11-14 15:14:20 -0800

    made template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
