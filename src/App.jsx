657f3efed2e2c7cbf8e6410a27f391483eaca273
Riley Wong <rileysw@uci.edu>
2021-11-14 17:36:29 -0800

    added frontend routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 /* eslint-disable no-alert */
 import React from 'react';
-import { Button, DatePicker } from 'antd';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Button, DatePicker } from 'antd';
 import './App.css';
 import 'antd/dist/antd.css';
 
+// Pages
+import Login from './pages/Login';
+import About from './pages/About';
+import Partners from './pages/Partners';
+import Solutions from './pages/Solutions';
+import Events from './pages/Events';
+import Volunteers from './pages/Volunteers';
+import Profile from './pages/Profile';
+import Event from './pages/Event';
+import Waivers from './pages/Waivers';
+
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/partners" exact element={<Partners />} />
+        <Route path="/solutions" exact element={<Solutions />} />
+        <Route path="/events" exact element={<Events />} />
+        <Route path="/volunteers" exact element={<Volunteers />} />
+        <Route path="/profile" exact element={<Profile />} />
+        <Route path="/event" exact element={<Event />} />
+        <Route path="/waivers" exact element={<Waivers />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+/*
+<div className="App">
       <header className="App-header">
         <p>
           *AntDesign provides a set of high-quality React components out of the box!*
@@ -23,7 +54,5 @@ function App() {
         <DatePicker placeholder="select date" />
       </header>
     </div>
-  );
-}
 
-export default App;
+*/
---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+
+function About() {
+  return <p>This is the about page.</p>;
+}
+
+export default About;
