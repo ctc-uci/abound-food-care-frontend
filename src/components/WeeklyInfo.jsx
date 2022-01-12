58717d336014c0fdd20795b60e0bd73faa037d6a
Madhu Sharma <madhuxsharma@gmail.com>
2022-01-12 13:58:44 -0800

    Switched to functional component

--- src/components/WeeklyInfo.jsx
+++ src/components/WeeklyInfo.jsx
@@ -1,92 +1,122 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Chart from 'react-apexcharts';
 
-class WeeklyInfo extends Component {
-  constructor(props) {
-    super(props);
+const WeeklyInfo = () => {
+  const [options, setOptions] = React.useState(null);
+  const [series, setSeries] = React.useState(null);
 
-    function generateData(count, yrange) {
-      const dayOfWeek = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-      ];
-      let i = 0;
-      const series = [];
-      while (i < count) {
-        const x = dayOfWeek[i];
-        const y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-        series.push({
-          x,
-          y,
-        });
-        i += 1;
-      }
-      return series;
-    }
-
-    // function onSquareClick(event, chartContext, config) {
-    //   console.log(event);
-    //   console.log(chartContext);
-    //   console.log(config);
-    // }
-
-    function generateSeries(startHour, endHour) {
-      const times = [];
-      for (let hour = startHour; hour <= endHour; hour += 1) {
-        const formattedHour = hour % 12 === 0 ? '12' : (hour % 12).toString();
-        const period = hour > 12 ? 'PM' : 'AM';
-        times.push(formattedHour.concat(':00').concat(period));
-        if (hour !== endHour) {
-          times.push(formattedHour.concat(':30').concat(period));
-        }
-      }
-      return times.reverse().map(time => {
-        return {
-          name: time,
-          data: generateData(7, {
-            min: 1,
-            max: 2,
-          }),
-        };
+  const generateData = count => {
+    const dayOfWeek = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    let i = 0;
+    const generatedSeries = [];
+    while (i < count) {
+      const x = dayOfWeek[i];
+      const y = 1;
+      generatedSeries.push({
+        x,
+        y,
       });
+      i += 1;
+    }
+    // console.log(generatedSeries);
+    return generatedSeries;
+  };
+
+  const generateSeries = (startHour, endHour) => {
+    const times = [];
+    for (let hour = startHour; hour <= endHour; hour += 1) {
+      const formattedHour = hour % 12 === 0 ? '12' : (hour % 12).toString();
+      const period = hour > 12 ? 'PM' : 'AM';
+      times.push(formattedHour.concat(':00').concat(period));
+      if (hour !== endHour) {
+        times.push(formattedHour.concat(':30').concat(period));
+      }
     }
 
-    this.state = {
-      options: {
-        chart: {
-          height: 350,
-          type: 'heatmap',
-          // events: {
-          //   dataPointSelection: onSquareClick,
-          // },
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        colors: ['#008FFB'],
-      },
-      series: generateSeries(9, 17),
-    };
-  }
+    const generatedSeries = times.reverse().map(time => {
+      return {
+        name: time,
+        data: generateData(7),
+      };
+    });
 
-  render() {
-    const { options, series } = this.state;
-    return (
-      <div className="app">
-        <h1>Weekly Availability</h1>
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart options={options} series={series} type="heatmap" width="500" height="500" />
-          </div>
-        </div>
+    setSeries(generatedSeries);
+  };
+
+  React.useEffect(() => {
+    console.log(series);
+  }, [series]);
+
+  const onSquareClick = (event, chartContext, config) => {
+    console.log(series);
+    // what square was clicked?
+    // seriesIndex = row (bottom = 0)
+    // dataPointIndex = column (left = 0)
+
+    // what value does it have?
+    // check if it's selected already or not
+
+    // toggle that value
+    // console.log(event);
+    // console.log(chartContext);
+    // console.log(config);
+    const rowIndex = config.seriesIndex;
+    const colIndex = config.dataPointIndex;
+    console.log(rowIndex, colIndex);
+    console.log(config);
+    console.log(config.w.config.series);
+    const updatedSeries = config.w.config.series;
+    console.log(updatedSeries);
+
+    console.log(updatedSeries[0].data[colIndex]);
+    // set y value to opposite
+    updatedSeries[0].data[colIndex].y = 100;
+    console.log(updatedSeries);
+    setSeries(updatedSeries);
+    // updatedSeries
+  };
+
+  React.useEffect(() => {
+    const values = {
+      chart: {
+        height: 350,
+        type: 'heatmap',
+        events: {
+          dataPointSelection: onSquareClick,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      colors: ['#008FFB'],
+    };
+    setOptions(values);
+    generateSeries(9, 17);
+  }, []);
+
+  const renderChart = () => {
+    if (options) {
+      return <Chart options={options} series={series} type="heatmap" width="500" height="500" />;
+    }
+    return null;
+  };
+
+  return (
+    <div className="app">
+      <h1>Weekly Availability</h1>
+      <div className="row">
+        <div className="mixed-chart"> {renderChart()} </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default WeeklyInfo;
