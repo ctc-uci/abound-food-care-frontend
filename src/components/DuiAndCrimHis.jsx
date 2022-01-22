d4ddcb1cdcee534ef87b5b11c5d7039497a16ec3
Rashmi Sharma <rashmisharma489@gmail.com>
2022-01-21 19:17:26 -0800

    adding headers, reformatting

--- src/components/DuiAndCrimHis.jsx
+++ src/components/DuiAndCrimHis.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Radio, Form, Input, Button } from 'antd';
 
-/* eslint-disable no-template-curly-in-string */
-
 const validateMessages = {
+  // eslint-disable-next-line no-template-curly-in-string
   required: '${label} is required!',
 };
-
-/* eslint-disable no-template-curly-in-string */
 
 const DuiAndCrimHis = () => {
   const [componentSize, setComponentSize] = React.useState('default');
@@ -17,42 +14,45 @@ const DuiAndCrimHis = () => {
   };
 
   return (
-    <Form
-      labelCol={{ span: 6 }}
-      wrapperCol={{ span: 14 }}
-      name="dui_criminal_history"
-      validateMessages={validateMessages}
-      size={componentSize}
-      onValuesChange={onFormLayoutChange}
-    >
-      <Form.Item label="Do you have a DUI (Driving Under Influence) history?" required>
-        <Radio.Group>
-          <Radio value>Yes</Radio>
-          <Radio value>No</Radio>
-        </Radio.Group>
-      </Form.Item>
+    <div>
+      <h1>DUI and Criminal History</h1>
+      <Form
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 14 }}
+        name="dui_criminal_history"
+        validateMessages={validateMessages}
+        size={componentSize}
+        onValuesChange={onFormLayoutChange}
+      >
+        <Form.Item label="Do you have a DUI (Driving Under Influence) history?" required>
+          <Radio.Group>
+            <Radio value>Yes</Radio>
+            <Radio value>No</Radio>
+          </Radio.Group>
+        </Form.Item>
 
-      <Form.Item label="Please elaborate if you checked Yes:">
-        <Input.TextArea />
-      </Form.Item>
+        <Form.Item label="Please elaborate if you checked Yes:">
+          <Input.TextArea />
+        </Form.Item>
 
-      <Form.Item label="Do you have a criminal history?" required>
-        <Radio.Group>
-          <Radio value>Yes</Radio>
-          <Radio value>No</Radio>
-        </Radio.Group>
-      </Form.Item>
+        <Form.Item label="Do you have a criminal history?" required>
+          <Radio.Group>
+            <Radio value>Yes</Radio>
+            <Radio value>No</Radio>
+          </Radio.Group>
+        </Form.Item>
 
-      <Form.Item label="Please elaborate if you checked Yes:">
-        <Input.TextArea />
-      </Form.Item>
+        <Form.Item label="Please elaborate if you checked Yes:">
+          <Input.TextArea />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 19 }}>
-        <Button type="primary" htmlType="submit">
-          Next
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 19 }}>
+          <Button type="primary" htmlType="submit">
+            Next
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
