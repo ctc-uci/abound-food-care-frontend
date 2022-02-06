a476ced23aa5328e39ab2fa331efec7a8c3bba0b
Maxim Alayev <neurologicalstyle@gmail.com>
2022-02-06 14:47:17 -0800

    Changed theme to green

--- src/pages/Volunteers.jsx
+++ src/pages/Volunteers.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import GeneralInfo from '../components/GeneralInfo';
 import WeeklyInfo from '../components/WeeklyInfo';
+import Database from '../components/Database';
 
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.variable.min.css';
 
 function Volunteers() {
   return (
@@ -10,6 +11,7 @@ function Volunteers() {
       <p>This is the volunteers page</p>
       <WeeklyInfo />
       <GeneralInfo />
+      <Database />
     </div>
   );
 }
