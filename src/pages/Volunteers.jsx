7442b0390f067568df4174c45e1ea683e7bcc9e7
Maxim Alayev <neurologicalstyle@gmail.com>
2022-02-14 14:07:54 -0800

    Formatted volunteer history

--- src/pages/Volunteers.jsx
+++ src/pages/Volunteers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GeneralInfo from '../components/GeneralInfo';
 import WeeklyInfo from '../components/WeeklyInfo';
 import 'antd/dist/antd.variable.min.css';
-import EditHours from '../components/EditHours';
+import VolunteeringHistory from '../components/VolunteeringHistory';
 
 function Volunteers() {
   return (
@@ -10,7 +10,7 @@ function Volunteers() {
       <p>This is the volunteers page</p>
       <WeeklyInfo />
       <GeneralInfo />
-      <EditHours />
+      <VolunteeringHistory />
     </div>
   );
 }
