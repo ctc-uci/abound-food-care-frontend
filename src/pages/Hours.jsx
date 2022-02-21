92f6090165e21d66a35519165c04bdb88e3cfcd7
Daniel Koo <donghyun122600@gmail.com>
2022-02-20 16:52:44 -0800

    cleaning

---
+++ src/pages/Hours.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import ReviewHours from '../components/ReviewHours';
+import 'antd/dist/antd.variable.min.css';
+
+function Hours() {
+  return (
+    <div>
+      <h1>Review Volunteer Hour Logs</h1>
+      <ReviewHours />
+    </div>
+  );
+}
+
+export default Hours;
