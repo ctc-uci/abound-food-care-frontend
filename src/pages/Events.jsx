7b89fa47b192aa542672c0da54f9c5d36ff052b1
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-03-01 11:17:37 -0600

    Admin: Create Event (#38)
    * Create PR for #31
    
    * Events General Info
    
    * test commit
    
    * connected additional info, deleting tags
    
    * added modals
    
    * added GeneralInfo
    
    * added GeneralInfo
    
    * updating yarn lockfile
    
    * color changes...
    
    * minor changes
    
    * tags
    
    * added required
    
    * updating yarn.lock
    
    * modal logic needs work
    
    * adding event type (partial)
    
    * changed tags to checkboxes (recent design change)
    
    * creating first modal, separate modals into diff files
    
    * new modals
    
    * edit event modal
    
    * added delete event type functionality, cleaning up code
    
    Co-authored-by: angelinepho <angelinepho@users.noreply.github.com>
    Co-authored-by: unknown <apho8884@gmail.com>
    Co-authored-by: Ivy Nguyen <ivynguyen2303@gmail.com>
    Co-authored-by: Rashmi Sharma <rashmisharma489@gmail.com>

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
+import { Button } from 'antd';
+import EventsGeneralInfo from '../components/EventsGeneralInfo';
+import EventsAdditionalInfo from '../components/EventsAdditionalInfo';
 
 function Events() {
-  return <p>This is the events page.</p>;
+  const [state, setState] = useState('general-info');
+
+  return (
+    <div>
+      <p>This is the events page.</p>
+      {state === 'general-info' && (
+        <>
+          <EventsGeneralInfo />
+          <div>
+            <Button
+              style={{
+                background: '#F5F5F5',
+                color: 'rgba(0, 0, 0, 0.25)',
+                borderColor: '#D9D9D9',
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => setState('additional-info')}
+              style={{
+                background: '#115740',
+                color: 'white',
+                borderColor: '#115740',
+                float: 'right',
+              }}
+            >
+              Next
+            </Button>
+          </div>
+        </>
+      )}
+
+      {state === 'additional-info' && (
+        <>
+          <EventsAdditionalInfo />
+          <div>
+            <Button
+              style={{
+                background: '#F5F5F5',
+                color: 'rgba(0, 0, 0, 0.25)',
+                borderColor: '#D9D9D9',
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              style={{
+                background: '#115740',
+                color: 'white',
+                borderColor: '#115740',
+                float: 'right',
+              }}
+            >
+              Publish Event
+            </Button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Events;
