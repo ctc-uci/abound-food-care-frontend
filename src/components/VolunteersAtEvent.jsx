5bd995bb7e886de74360487b42c43c1f7b2e0e55
Maxim Alayev <neurologicalstyle@gmail.com>
2022-03-13 13:39:04 -0700

    Changes

--- src/components/VolunteersAtEvent.jsx
+++ src/components/VolunteersAtEvent.jsx
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Table, ConfigProvider } from 'antd';
 import './eventPage.css';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 const VolunteersAtEvent = props => {
   const { name, type, eventId } = props;
+  const [volunteers, setVolunteers] = useState([]);
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/volunteers/getVolunteers/${eventId}`)
+      .then(res => {
+        for (let i = 0; i < res.data.length; i += 1) {
+          res.data[i].name = `${res.data[i].first_name} ${res.data[i].last_name}`;
+        }
+        setVolunteers(res.data);
+        let emailM = 'mailto:';
+        for (let i = 0; i < res.data.length; i += 1) {
+          emailM += `${res.data[i].email};`;
+        }
+        setEmail(emailM);
+      })
+      .then(() => {});
+  }, []);
 
   ConfigProvider.config({
     theme: {
       primaryColor: '#115740',
     },
   });
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => (
+        <a style={{ color: '#115740' }} href="/volunteers">
+          {text}
+        </a>
+      ),
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone',
+      key: 'phone',
+    },
+    {
+      title: 'Waiver',
+      dataIndex: 'waiver',
+      key: 'waiver',
+      render: () => {
+        return (
+          <a style={{ color: '#115740' }} href={email}>
+            Download
+          </a>
+        );
+      },
+    },
+  ];
 
   return (
     <ConfigProvider>
@@ -69,14 +124,19 @@ const VolunteersAtEvent = props => {
               flexWrap: 'wrap',
             }}
           >
-            <Button type="primary" style={{ marginRight: '2vw' }}>
+            <Button
+              type="primary"
+              style={{ marginRight: '2vw' }}
+              onClick={() => {
+                window.location = email;
+              }}
+            >
               Email Volunteers
             </Button>
             <Button type="primary">Download All Waivers</Button>
           </div>
         </div>
-        <p> {eventId}</p>
-        <Table />
+        <Table dataSource={volunteers} columns={columns} />
       </div>
     </ConfigProvider>
   );
