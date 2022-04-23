5d644db623618c2e0dabe462222ef7d6217b2651
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-04-23 17:37:58 -0500

    create-event-mobile (#76)
    * Create PR for #67
    
    * Made mobile view for create event
    
    * Added mobile view for Additional Information
    
    * Added CSS
    
    * Added CSS and added Mobile Views
    
    * changed inputnumber to input
    
    * adding rhf to mobile, debugging
    
    Co-authored-by: ivynguyen2303 <ivynguyen2303@users.noreply.github.com>
    Co-authored-by: Madhu Sharma <madhuxsharma@gmail.com>
    Co-authored-by: henryg0 <hgip@uci.edu>
    Co-authored-by: Rashmi Sharma <rashmisharma489@gmail.com>

--- src/components/events/createEvent/EventsAdditionalInfo.jsx
+++ src/components/events/createEvent/EventsAdditionalInfo.jsx
@@ -2,48 +2,102 @@ import React from 'react';
 import { useFormContext, Controller } from 'react-hook-form';
 import { Form, Input, Upload, Button } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
+import useViewPort from '../../../common/useViewPort';
+import './createEvent.css';
+
+const { TextArea } = Input;
 
 const EventsAdditionalInfo = () => {
+  const { width } = useViewPort();
+  const breakpoint = 720;
   const { control } = useFormContext();
 
   return (
     <div>
-      <h1> Additional Information </h1>
-      <Controller
-        control={control}
-        name="notes"
-        render={({ field: { onChange, value, ref } }) => (
-          <Form.Item label="Additional Info">
-            <Input
-              placeholder="Ex. This event will take place on December 3, 2021 at 9:00AM."
-              ref={ref}
-              onChange={onChange}
-              value={value}
-            />
-          </Form.Item>
-        )}
-      />
-      <Controller
-        control={control}
-        name="fileAttachments"
-        render={({ field: { onChange, ref } }) => (
-          <Form.Item label="Upload Forms">
-            <Upload multiple ref={ref} onChange={onChange}>
-              <Button
-                icon={<RightOutlined />}
-                style={{
-                  background: 'rgba(108, 194, 74, 0.25)',
-                  color: 'rgba(0, 0, 0, 0.85)',
-                  border: 'rgba(17, 87, 64, 0.25)',
-                }}
-              >
-                {' '}
-                Click to Upload
-              </Button>
-            </Upload>
-          </Form.Item>
-        )}
-      />
+      <h1> Additional Info </h1>
+      <div className="description">
+        <h2>Include additional information you would like your volunteers to know.</h2>
+      </div>
+      {width < breakpoint ? (
+        <>
+          <br />
+          <Controller
+            control={control}
+            name="notes"
+            render={({ field: { onChange, value, ref } }) => (
+              <Form.Item label="Additional Info">
+                <TextArea
+                  placeholder="Ex: This event will take place on December 3, 2021 at 9:00AM"
+                  autoSize={{ minRows: 7, maxRows: 7 }}
+                  ref={ref}
+                  onChange={onChange}
+                  value={value}
+                />
+              </Form.Item>
+            )}
+          />
+          <Controller
+            control={control}
+            name="fileAttachments"
+            render={({ field: { onChange, ref } }) => (
+              <Form.Item label="Upload Forms">
+                <Upload multiple ref={ref} onChange={onChange}>
+                  <Button
+                    icon={<RightOutlined />}
+                    style={{
+                      background: 'rgba(108, 194, 74, 0.25)',
+                      color: 'rgba(0, 0, 0, 0.85)',
+                      border: 'rgba(17, 87, 64, 0.25)',
+                    }}
+                  >
+                    {' '}
+                    Click to Upload
+                  </Button>
+                </Upload>
+              </Form.Item>
+            )}
+          />
+        </>
+      ) : (
+        <div>
+          <h1> Additional Information </h1>
+          <Controller
+            control={control}
+            name="notes"
+            render={({ field: { onChange, value, ref } }) => (
+              <Form.Item label="Additional Info">
+                <Input
+                  placeholder="Ex. This event will take place on December 3, 2021 at 9:00AM."
+                  ref={ref}
+                  onChange={onChange}
+                  value={value}
+                />
+              </Form.Item>
+            )}
+          />
+          <Controller
+            control={control}
+            name="fileAttachments"
+            render={({ field: { onChange, ref } }) => (
+              <Form.Item label="Upload Forms">
+                <Upload multiple ref={ref} onChange={onChange}>
+                  <Button
+                    icon={<RightOutlined />}
+                    style={{
+                      background: 'rgba(108, 194, 74, 0.25)',
+                      color: 'rgba(0, 0, 0, 0.85)',
+                      border: 'rgba(17, 87, 64, 0.25)',
+                    }}
+                  >
+                    {' '}
+                    Click to Upload
+                  </Button>
+                </Upload>
+              </Form.Item>
+            )}
+          />
+        </div>
+      )}
     </div>
   );
 };
