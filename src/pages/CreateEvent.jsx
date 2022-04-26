b2ee3501365eb2d3af9b00f5e1a9c67efe015ea3
Rashmi Sharma <rashmisharma489@gmail.com>
2022-04-26 16:58:35 -0700

    removing consolelog and comments

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -116,11 +116,7 @@ const CreateEvent = () => {
       methods.setValue('eventEndDate', moment(endDateTime));
       methods.setValue('eventStartTime', moment(startDateTime));
       methods.setValue('waivers', eventData.waivers ? eventData.waivers : []);
-      console.log(eventData);
       if (eventData.requirements) {
-        // console.log(requirements);
-        // requirements = requirements.replaceAll('"', ''); // requirements that are two words are returned in quotes - must remove
-        // requirements = requirements.slice(1, requirements.length - 1).split(',');
         eventData.requirements.forEach(r => setRequirements(r));
       }
     } catch (e) {
