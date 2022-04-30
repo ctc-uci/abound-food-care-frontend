9565fd32dd54a9b461bad3ee3d75a64d5f67a59b
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-04-30 15:35:32 -0500

    rv admin dashboard mobile view (#100)
    * Create PR for #67
    
    * Made mobile view for create event
    
    * Added mobile view for Additional Information
    
    * Added CSS
    
    * Added CSS and added Mobile Views
    
    * changed inputnumber to input
    
    * Created Mobile view for Admin dashboard
    
    * Edited mobile view
    
    * Added mobile view for Admin Dashbaord
    
    * Added mobile view for Admin Dashbaord
    
    * resolving comments
    
    Co-authored-by: ivynguyen2303 <ivynguyen2303@users.noreply.github.com>
    Co-authored-by: Madhu Sharma <madhuxsharma@gmail.com>
    Co-authored-by: henryg0 <hgip@uci.edu>
    Co-authored-by: Rashmi Sharma <rashmisharma489@gmail.com>

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,24 +1,50 @@
-import '../styles/AdminDashboard.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Row, Col } from 'antd';
-import React from 'react';
 import AdminNotifications from '../components/admin-dashboard-components/AdminNotifications';
 import DashboardHeader from '../components/admin-dashboard-components/DashboardHeader';
 import EventGrid from '../components/admin-dashboard-components/EventGrid';
+import EventList from '../components/events/EventList';
+import useViewPort from '../common/useViewPort';
+import '../styles/AdminDashboard.css';
 
 const AdminDashboard = () => {
-  // HARDCODED USERID FOR NOW
+  const { width } = useViewPort();
+  const breakpoint = 720;
+
+  const [pastEvents, setPastEvents] = useState([]);
+  const [upcomingEvents, setUpcomingEvents] = useState([]);
+
+  useEffect(async () => {
+    const uEvents = await axios.get('http://localhost:3001/events/upcoming');
+    setUpcomingEvents(uEvents.data);
+    const pEvents = await axios.get('http://localhost:3001/events/past');
+    setPastEvents(pEvents.data);
+  }, []);
+
   return (
     <div className="dashboard-container">
       <DashboardHeader isAdmin userId={2} />
-      <Row className="dashboard-row" gutter={[32, 16]}>
-        <Col className="dashboard-col" span={18}>
-          <EventGrid title="Upcoming Events" eventStatus="upcoming" />
-          <AdminNotifications />
-        </Col>
-        <Col className="dashboard-col" span={6}>
-          <EventGrid title="Past Events" eventStatus="past" />
-        </Col>
-      </Row>
+      {width > breakpoint ? (
+        <>
+          <Row className="dashboard-row" gutter={[32, 16]}>
+            <Col className="dashboard-col" span={18}>
+              <EventGrid title="Upcoming Events" eventStatus="upcoming" />
+              <AdminNotifications />
+            </Col>
+            <Col className="dashboard-col" span={6}>
+              <EventGrid title="Past Events" eventStatus="past" />
+            </Col>
+          </Row>
+        </>
+      ) : (
+        <>
+          <div>
+            <EventList title="Upcoming Events" events={upcomingEvents} showViewAll />
+            <EventList title="Past Events" events={pastEvents} showViewAll />
+          </div>
+        </>
+      )}
     </div>
   );
 };
