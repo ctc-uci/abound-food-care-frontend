ac0bc7d7aebdb9b2a2e30441d587ff4be63977a7
jeffcheema <jeffcheema007@gmail.com>
2022-07-11 13:33:44 -0700

    Fixed formatting on Login and Sign up pages to match figma

--- src/components/navigation/AdminNavMenu.jsx
+++ src/components/navigation/AdminNavMenu.jsx
@@ -1,5 +1,5 @@
 import React, { useState, createElement } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Menu, Layout, Button } from 'antd';
 import {
   ProfileOutlined,
@@ -17,7 +17,8 @@ const { Sider } = Layout;
 const AdminNavMenu = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [menuTitle, setMenuTitle] = useState('Abound Food Care');
-
+  const { pathname } = useLocation();
+  console.log({ pathname });
   const handleToggle = () => {
     setCollapsed(!collapsed);
     if (!collapsed) {
@@ -28,49 +29,53 @@ const AdminNavMenu = () => {
   };
 
   return (
-    <div style={{ position: 'relative' }}>
-      <Sider
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
-        style={{ height: '100vh', backgroundColor: 'var(--eden)', color: 'white' }}
-      >
-        <Link to="/">
-          <div className="logo">
-            <img src={Logo} className="logo-img" alt="AFC Logo" />
-            {menuTitle}
-          </div>
-        </Link>
-        <Menu defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item className="menu-item" key="1" icon={<DashboardOutlined />}>
-            <Link to="/admin" className="link">
-              Dashboard
+    <>
+      {pathname !== '/' && (
+        <div style={{ position: 'relative' }}>
+          <Sider
+            trigger={null}
+            collapsible
+            collapsed={collapsed}
+            style={{ height: '100vh', backgroundColor: 'var(--eden)', color: 'white' }}
+          >
+            <Link to="/">
+              <div className="logo">
+                <img src={Logo} className="logo-img" alt="AFC Logo" />
+                {menuTitle}
+              </div>
             </Link>
-          </Menu.Item>
-          <Menu.Item className="menu-item" key="2" icon={<FormOutlined />}>
-            <Link to="/events" className="link">
-              Events
-            </Link>
-          </Menu.Item>
-          <Menu.Item className="menu-item" key="3" icon={<TableOutlined />}>
-            <Link to="/volunteers" className="link">
-              Volunteers
-            </Link>
-          </Menu.Item>
-          <Menu.Item className="menu-item" key="4" icon={<ProfileOutlined />}>
-            <Link to="/hours" className="link">
-              Hours Log
-            </Link>
-          </Menu.Item>
-        </Menu>
-        <Button onClick={() => handleToggle} className="toggle-btn">
-          {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: handleToggle,
-          })}
-        </Button>
-      </Sider>
-    </div>
+            <Menu defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item className="menu-item" key="1" icon={<DashboardOutlined />}>
+                <Link to="/admin" className="link">
+                  Dashboard
+                </Link>
+              </Menu.Item>
+              <Menu.Item className="menu-item" key="2" icon={<FormOutlined />}>
+                <Link to="/events" className="link">
+                  Events
+                </Link>
+              </Menu.Item>
+              <Menu.Item className="menu-item" key="3" icon={<TableOutlined />}>
+                <Link to="/volunteers" className="link">
+                  Volunteers
+                </Link>
+              </Menu.Item>
+              <Menu.Item className="menu-item" key="4" icon={<ProfileOutlined />}>
+                <Link to="/hours" className="link">
+                  Hours Log
+                </Link>
+              </Menu.Item>
+            </Menu>
+            <Button onClick={() => handleToggle} className="toggle-btn">
+              {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick: handleToggle,
+              })}
+            </Button>
+          </Sider>
+        </div>
+      )}
+    </>
   );
 };
 
