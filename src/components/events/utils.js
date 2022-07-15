d67d965407f3154fd07759419fb0575bd0228522
Alan Chang <alan@alanchang.xyz>
2022-07-14 21:05:24 -0700

    Add filename param to define S3 link download name

--- src/components/events/utils.js
+++ src/components/events/utils.js
@@ -3,7 +3,9 @@ import { AFCBackend } from '../../util/utils';
 
 const uploadWaiver = async file => {
   // get S3 upload url from server
-  const { data: uploadUrl } = await AFCBackend.get(`/s3Upload`);
+  const { data: uploadUrl } = await AFCBackend.get(`/s3Upload`, {
+    params: { filename: file.name },
+  });
   // upload image directly to S3 bucket
   await axios.put(uploadUrl, file, {
     headers: {
@@ -11,8 +13,8 @@ const uploadWaiver = async file => {
     },
   });
   // return box image url
-  const imageUrl = uploadUrl.split('?')[0];
-  return imageUrl;
+  const waiverUrl = uploadUrl.split('?')[0];
+  return waiverUrl;
 };
 
 export default uploadWaiver;
