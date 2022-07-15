75deacdc8803557885bc92afd5c45f3faf0bbd9f
Alan Chang <alan@alanchang.xyz>
2022-07-14 21:31:16 -0700

    Change waiver upload to set content disposition header in put file request instead of get signed url request

--- src/components/events/utils.js
+++ src/components/events/utils.js
@@ -3,13 +3,12 @@ import { AFCBackend } from '../../util/utils';
 
 const uploadWaiver = async file => {
   // get S3 upload url from server
-  const { data: uploadUrl } = await AFCBackend.get(`/s3Upload`, {
-    params: { filename: file.name },
-  });
+  const { data: uploadUrl } = await AFCBackend.get(`/s3Upload`);
   // upload image directly to S3 bucket
   await axios.put(uploadUrl, file, {
     headers: {
       'Content-Type': 'multipart/form-data',
+      'Content-Disposition': `attachment; filename="${file.name}"`,
     },
   });
   // return box image url
