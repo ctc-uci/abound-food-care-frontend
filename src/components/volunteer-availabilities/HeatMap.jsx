6c794a39d1b71e08aef04a375607759695d01b81
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-10-07 02:59:18 -0700

    Implement volunteer heatmap (#160)
    * Create PR for #159
    
    * Use backend data in volunteer heatmap, cleanup code
    
    * Select heatmap volunteers iteration 1
    
    * Fix toggle bug
    
    Co-authored-by: jrock-3 <jrock-3@users.noreply.github.com>
    Co-authored-by: jrock03 <liaojy2@uci.edu>
    Co-authored-by: jrock-3 <justin.liao3@gmail.com>

--- src/components/volunteer-availabilities/HeatMap.jsx
+++ src/components/volunteer-availabilities/HeatMap.jsx
@@ -1,64 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
-// import ApexCharts from 'apexcharts';
-import { AFCBackend } from '../../util/utils';
+import { PropTypes } from 'prop-types';
+import { AFCBackend, dayOfWeek } from '../../util/utils';
 
-const HeatMap = () => {
-  const [options, setOptions] = React.useState(null);
-  const [series, setSeries] = React.useState([]);
+const afterTimes = {
+  '08:00AM': '08:30AM',
+  '08:30AM': '09:00AM',
+  '09:00AM': '09:30AM',
+  '09:30AM': '10:00AM',
+  '10:00AM': '10:30AM',
+  '10:30AM': '11:00AM',
+  '11:00AM': '11:30AM',
+  '11:30AM': '12:00PM',
+  '12:00PM': '12:30PM',
+  '12:30PM': '13:00PM',
+  '13:00PM': '13:30PM',
+  '13:30PM': '14:00PM',
+  '14:00PM': '14:30PM',
+  '14:30PM': '15:00PM',
+  '15:00PM': '15:30PM',
+  '15:30PM': '16:00PM',
+  '16:00PM': '16:30PM',
+  '16:30PM': '17:00PM',
+  '17:00PM': '17:30PM',
+};
 
-  const generateData = (count, startHour, endHour, data) => {
-    const dayOfWeek = [
-      'sunday',
-      'monday',
-      'tuesday',
-      'wednesday',
-      'thursday',
-      'friday',
-      'saturday',
-    ];
-    let i = 0;
-    const generatedSeries = [];
-    while (i < count) {
-      const x = dayOfWeek[i];
+const timePairs = Object.entries(afterTimes).map(([start, end]) => [
+  start.substring(0, 5),
+  end.substring(0, 5),
+]);
+
+const HeatMap = ({ setSelectedTimeslot }) => {
+  const [options, setOptions] = useState(null);
+  const [series, setSeries] = useState([]);
+
+  const generateData = (startHour, endHour, data) => {
+    return dayOfWeek.map(x => {
       const timestring = `${x} ${startHour.substring(0, 5)} to ${endHour.substring(0, 5)}`;
-      const y = data[timestring] ? data[timestring] + 1 : 1; // value of each cell, remove + 1, set default as 0 during prod
-      generatedSeries.push({
-        x,
-        y,
-      });
-      i += 1;
-    }
-    return generatedSeries;
+      const y = data[timestring] ? data[timestring] : 0; // value of each cell, remove + 1, set default as 0 during prod
+      return { x, y };
+    });
   };
 
   const generateSeries = async (startHour, endHour) => {
     const times = [];
-    const afterTimes = {
-      '08:00AM': '08:30AM',
-      '08:30AM': '09:00AM',
-      '09:00AM': '09:30AM',
-      '09:30AM': '10:00AM',
-      '10:00AM': '10:30AM',
-      '10:30AM': '11:00AM',
-      '11:00AM': '11:30AM',
-      '11:30AM': '12:00PM',
-      '12:00PM': '12:30PM',
-      '12:30PM': '1:00PM',
-      '13:00PM': '13:30PM',
-      '13:30PM': '14:00PM',
-      '14:00PM': '14:30PM',
-      '14:30PM': '15:00PM',
-      '15:00PM': '15:30PM',
-      '15:30PM': '16:00PM',
-      '16:00PM': '16:30PM',
-      '16:30PM': '17:00PM',
-      '17:00PM': '17:30PM',
-    };
-    let data = {};
-    await AFCBackend.get('/volunteers/available').then(res => {
-      data = res.data;
-    });
+    const { data } = await AFCBackend.get('/volunteers/available');
     for (let hour = startHour; hour <= endHour; hour += 1) {
       const formattedHour = hour < 10 ? `0${String(hour)}` : String(hour);
       const period = hour >= 12 ? 'PM' : 'AM';
@@ -68,16 +54,32 @@ const HeatMap = () => {
       }
     }
 
-    const generatedSeries = times.reverse().map(time => {
-      return {
-        name: time,
-        data: generateData(7, time, afterTimes[time], data),
-      };
-    });
+    const generatedSeries = times.reverse().map(time => ({
+      name: time,
+      data: generateData(time, afterTimes[time], data),
+    }));
     await setSeries(generatedSeries);
   };
 
-  React.useEffect(() => {
+  const onSquareClick = async (event, chartContext, config) => {
+    // Get square coordinates
+    const rowIndex = config.seriesIndex;
+    const colIndex = config.dataPointIndex;
+
+    // Convert to readable date and time
+    const day = dayOfWeek[colIndex];
+    const time = timePairs[18 - rowIndex];
+
+    // Update selected time
+    setSelectedTimeslot(prev => {
+      if (prev.day === day && prev?.time && prev.time[0] === time[0] && prev.time[1] === time[1]) {
+        return {};
+      }
+      return { day, time };
+    });
+  };
+
+  useEffect(() => {
     const values = {
       chart: {
         id: 'availability',
@@ -93,6 +95,11 @@ const HeatMap = () => {
             speed: 5,
           },
         },
+        events: {
+          dataPointSelection: async (event, chartContext, config) => {
+            await onSquareClick(event, chartContext, config);
+          },
+        },
       },
       dataLabels: {
         enabled: false,
@@ -106,18 +113,13 @@ const HeatMap = () => {
     generateSeries(9, 17);
   }, []);
 
-  const renderChart = () => {
-    if (options) {
-      return <Chart options={options} series={series} type="heatmap" width="800" height="500" />;
-    }
-    return null;
-  };
-
   return (
-    <div>
-      <div className="mixed-chart"> {renderChart()} </div>
-    </div>
+    options && <Chart options={options} series={series} type="heatmap" width="800" height="500" />
   );
+};
+
+HeatMap.propTypes = {
+  setSelectedTimeslot: PropTypes.func.isRequired,
 };
 
 export default HeatMap;
--- src/components/volunteer-availabilities/VolunteerAvailability.jsx
+++ src/components/volunteer-availabilities/VolunteerAvailability.jsx
@@ -1,157 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import HeatMap from './HeatMap';
+import { AFCBackend } from '../../util/utils';
 
-const VolunteerAvailability = props => {
-  const { handleViewDatabase } = props;
-  const VOLUNTEERS_DUMMY_DATA = [
-    {
-      id: 0,
-      name: 'John Stone',
-      available: true,
-    },
-    {
-      id: 1,
-      name: 'Ponnappa Priya',
-      available: true,
-    },
-    {
-      id: 2,
-      name: 'Mia Wong',
-      available: true,
-    },
-    {
-      id: 3,
-      name: 'Peter Stanbridge',
-      available: true,
-    },
-    {
-      id: 4,
-      name: 'Natalie Lee-Walsh',
-      available: true,
-    },
-    {
-      id: 5,
-      name: 'Ang Li',
-      available: true,
-    },
-    {
-      id: 6,
-      name: 'Nguta Ithya',
-      available: true,
-    },
-    {
-      id: 7,
-      name: 'John Stone',
-      available: true,
-    },
-    {
-      id: 8,
-      name: 'Ponnappa Priya',
-      available: true,
-    },
-    {
-      id: 9,
-      name: 'Mia Wong',
-      available: true,
-    },
-    {
-      id: 10,
-      name: 'Peter Stanbridge',
-      available: true,
-    },
-    {
-      id: 11,
-      name: 'Natalie Lee-Walsh',
-      available: true,
-    },
-    {
-      id: 12,
-      name: 'Ang Li',
-      available: true,
-    },
-    {
-      id: 13,
-      name: 'Nguta Ithya',
-      available: true,
-    },
-    {
-      id: 14,
-      name: 'John Stone',
-      available: true,
-    },
-    {
-      id: 15,
-      name: 'Ponnappa Priya',
-      available: true,
-    },
-    {
-      id: 16,
-      name: 'Mia Wong',
-      available: true,
-    },
-    {
-      id: 17,
-      name: 'Peter Stanbridge',
-      available: true,
-    },
-    {
-      id: 18,
-      name: 'Natalie Lee-Walsh',
-      available: true,
-    },
-    {
-      id: 19,
-      name: 'Ang Li',
-      available: true,
-    },
-    {
-      id: 20,
-      name: 'Nguta Ithya',
-      available: true,
-    },
-    {
-      id: 21,
-      name: 'John Stone',
-      available: false,
-    },
-    {
-      id: 22,
-      name: 'Ponnappa Priya',
-      available: false,
-    },
-    {
-      id: 23,
-      name: 'Mia Wong',
-      available: false,
-    },
-    {
-      id: 24,
-      name: 'Peter Stanbridge',
-      available: false,
-    },
-    {
-      id: 25,
-      name: 'Natalie Lee-Walsh',
-      available: false,
-    },
-    {
-      id: 26,
-      name: 'Ang Li',
-      available: false,
-    },
-  ];
-
-  const showDatabase = () => {
-    handleViewDatabase();
-  };
+const VolunteerAvailability = ({ handleViewDatabase }) => {
+  const [selectedTimeslot, setSelectedTimeslot] = useState({});
   const [volunteers, setVolunteers] = useState([]);
   const [availableVolunteers, setAvailableVolunteers] = useState([]);
+  const [filteredVolunteers, setFilteredVolunteers] = useState([]);
 
-  useEffect(() => {
-    setVolunteers(VOLUNTEERS_DUMMY_DATA);
-    setAvailableVolunteers(VOLUNTEERS_DUMMY_DATA.filter(v => v.available));
+  useEffect(async () => {
+    const { data } = await AFCBackend.get('/volunteers/');
+    const mappedVolunteers = data.map(({ userId, firstName, lastName, availabilities }) => ({
+      id: userId,
+      firstName,
+      lastName,
+      available: !!availabilities,
+    }));
+    setVolunteers(mappedVolunteers);
+    const availables = mappedVolunteers.filter(v => v.available);
+    setAvailableVolunteers(availables);
+    setFilteredVolunteers(availables);
   }, []);
+
+  useEffect(async () => {
+    const { day, time } = selectedTimeslot;
+    if (selectedTimeslot === {} || !day || !time) {
+      setFilteredVolunteers(availableVolunteers);
+      return;
+    }
+    const [start, end] = time;
+    const { data } = await AFCBackend.get(
+      `volunteers/available/day/${day}/start/${start}/end/${end}`,
+    );
+    const processedData = data.map(e => {
+      const { first_name: firstName, last_name: lastName, user_id: id } = e;
+      return { id, firstName, lastName };
+    });
+    setFilteredVolunteers(processedData);
+  }, [selectedTimeslot]);
 
   return (
     <div className="volunteer-availabilities">
@@ -166,7 +53,7 @@ const VolunteerAvailability = props => {
               id="search-volunteers"
             />
             <div className="right-align">
-              <button type="button" onClick={showDatabase}>
+              <button type="button" onClick={handleViewDatabase}>
                 View Database
               </button>
               <button type="button">Export</button>
@@ -196,22 +83,17 @@ const VolunteerAvailability = props => {
             </label>
           </div>
         </div>
-        <HeatMap />
+        <HeatMap setSelectedTimeslot={setSelectedTimeslot} />
       </div>
       <div className="available-volunteers">
         <h2>
-          Volunteers ({availableVolunteers.length}/{volunteers.length})
+          Volunteers ({filteredVolunteers.length}/{volunteers.length})
         </h2>
-        {availableVolunteers.map(v => {
-          const nameArr = v.name.split(' ');
-          const lastName = nameArr[1];
-          const firstName = nameArr[0];
-          return (
-            <p key={v.id}>
-              {lastName}, {firstName}
-            </p>
-          );
-        })}
+        {filteredVolunteers.map(({ id, firstName, lastName }) => (
+          <p key={id}>
+            {lastName}, {firstName}
+          </p>
+        ))}
       </div>
     </div>
   );
