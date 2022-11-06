aa3aae1db0f41711073cdd7b6a5143268876bce5
Madhu2244 <madhuxsharma@gmail.com>
2022-11-06 14:32:39 -0800

    Added CSS + Prettified the Front end UI

--- src/pages/Hours.jsx
+++ src/pages/Hours.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { Table, Input, Button } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Table, Input, Button, Typography, Dropdown, Space } from 'antd';
+import { DownOutlined, SearchOutlined } from '@ant-design/icons';
+import './Hours.css';
+
+const { Title } = Typography;
 
 const Hours = () => {
   const [selectedRows, setSelectedRows] = React.useState([]);
+
+  const items = [
+    {
+      label: 'Submit and continue',
+      key: '1',
+    },
+  ];
+
+  const [loadings, setLoadings] = useState([]);
+
+  const enterLoading = index => {
+    setLoadings(state => {
+      const newLoadings = [...state];
+      newLoadings[index] = true;
+      return newLoadings;
+    });
+    setTimeout(() => {
+      setLoadings(state => {
+        const newLoadings = [...state];
+        newLoadings[index] = false;
+        return newLoadings;
+      });
+    }, 6000);
+  };
 
   const columns = [
     {
@@ -22,6 +49,11 @@ const Hours = () => {
       key: 'event_name',
     },
     {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
       title: 'Start (PST)',
       dataIndex: 'start',
       key: 'start',
@@ -36,10 +68,10 @@ const Hours = () => {
       dataIndex: 'approval',
       key: 'approval',
       render: () => (
-        <>
+        <Space>
+          <Button type="secondary"> Decline </Button>
           <Button type="primary"> Approve </Button>
-          <Button type="primary"> Decline </Button>
-        </>
+        </Space>
       ),
     },
   ];
@@ -49,18 +81,19 @@ const Hours = () => {
   for (let i = 0; i < 20; i += 1) {
     data.push({
       key: i,
-      user: i,
-      organizations: 'CTC',
-      event_name: 'All-Hands',
-      start: '11/6/2022',
-      end: '11/7/2022',
-      approval: 'Approved',
+      user: 'FirstName LastName',
+      organizations: 'Organizationname',
+      event_name: 'EventName',
+      date: '01/01/2021',
+      start: '9:00 am',
+      end: '11:00 am',
     });
   }
 
   const rowSelection = {
     onChange: selectedRowKeys => {
       setSelectedRows(selectedRowKeys);
+      selectedRows.clear();
     },
     getCheckboxProps: record => ({
       disabled: record.name === 'Disabled User',
@@ -69,30 +102,52 @@ const Hours = () => {
     }),
   };
 
+  const searchBar = () => {
+    return (
+      <div>
+        <div>
+          <Input
+            prefix={<SearchOutlined style={{ color: '#BFBFBF' }} />}
+            size="large"
+            placeholder="Search by name, email, role..."
+            onPressEnter={null}
+            onChange={null}
+            allowClear
+          />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1>Review Volunteer Hour Logs</h1>
-      <div>
-        <Input
-          prefix={<SearchOutlined style={{ color: '#BFBFBF' }} />}
-          size="large"
-          placeholder="Search by name, email, role..."
-          onPressEnter={null}
-          onChange={null}
-          allowClear
+      <Title>Review Volunteer Hour Logs</Title>
+      <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+        <div className="search-bar">
+          {searchBar()}
+          <div className="filters">
+            <Title level={3}>Sort by: &nbsp;</Title>
+            <Dropdown.Button
+              icon={<DownOutlined />}
+              loading={loadings[1]}
+              menu={{
+                items,
+              }}
+              onClick={() => enterLoading(1)}
+            >
+              Date (most recent first)
+            </Dropdown.Button>
+          </div>
+        </div>
+        <Table
+          rowSelection={{
+            type: 'checkbox',
+            ...rowSelection,
+          }}
+          columns={columns}
+          dataSource={data}
         />
-      </div>
-      <Button type="primary" disabled={selectedRows === 0}>
-        Primary Button
-      </Button>
-      <Table
-        rowSelection={{
-          type: 'checkbox',
-          ...rowSelection,
-        }}
-        columns={columns}
-        dataSource={data}
-      />
+      </Space>
     </div>
   );
 };
